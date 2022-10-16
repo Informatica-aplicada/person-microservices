490eb8e7ddb31234684d44e412b2fd7fbc84f42b
DESKTOP-OV32B42\Usuario <shesly.obando@ucr.ac.cr>
2022-10-16 09:47:54 -0600

    crud parte 1

--- Interface/IPerson.cs
+++ Interface/IPerson.cs
@@ -3,6 +3,9 @@ using apiPersonaNet.Models;
 public interface IPerson
 {
     List<PersonInfo>  getPersonList(int[] ids);
+
+     public List<PersonInfo> getAllThePeople();
+
     UserModel auth(LoginCredentials auth);
 
 }
